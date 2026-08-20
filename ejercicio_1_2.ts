6f6dc17b7ef717e575919f93b2793bedf8381126
David Ruales <hruales1138@gmail.com>
2026-08-20 16:49:10 -0500

    c

---
+++ ejercicio_1_2.ts
@@ -0,0 +1,11 @@
+// 1. Crear un array con 5 nombres de estudiantes y mostrar cada nombre en consola usando un ciclo.
+
+let names: string[] = ['Tomas', 'Sara', 'Kevin', 'Juan', 'David']
+
+for (let name of names) {
+    console.log(name)
+}
+
+// 2. Imprimir cuantos estudiantes hay en el arreglo.
+
+console.log('Numero de estudiantes: ' + names.length)
---
+++ ejercico_3.ts
@@ -0,0 +1,8 @@
+// 3. Crear un array de números y calcular la suma total
+
+let nums = [1,2,3,4]
+let sum = 0
+
+for (let num in nums) {
+    sum += num
+}
